b9df7a1668e8caa66e50d6b3e1d9bf361c230a7e
jack198417 <j.atadjanov@gmail.com>
2021-04-03 23:50:19 +0500

    Create task2.go

---
+++ task2.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"errors"
+	"strings"
+	"strconv"
+)
+
+func decode(str string) (res string) {
+	rx := regexp.MustCompile(`\#\d+\#`)
+	fmt.Println(str, ":", rx.MatchString(str));
+	nums  := rx.FindAllString(str, -1)
+
+	if len(nums) < 1 {
+	 return str;
+	}
+
+	items := rx.Split(str , -1)
+	res = items[0]
+	for i := 0; i < len(nums); i++ {
+		n, _ := strconv.ParseInt(strings.Replace(nums[i], "#", "", -1), 10, 0)
+		ch := string(items[i + 1][0]);
+		for j := 0; j < int(n); j++ {
+			res +=	ch;
+		}
+		res += items[i + 1]
+	}
+
+	return;
+}
+
+func encode(str string) (string, error) {
+	rx := regexp.MustCompile(`\#\d+\#`)
+	if rx.MatchString(str) {
+	  return str, errors.New("This string can not be Compressed by this Algorithm")
+	}
+	
+	n := len(str)
+	p := 0;
+	cnt := 1;
+	ch := string(str[p])
+	res := "";
+
+	for i := 1; i < n; i++{
+		if ch == string(str[i]){
+		  cnt++;
+		  continue;
+		}
+
+		if cnt > 4 {
+		  res += "#" + strconv.Itoa(cnt) + "#" + ch
+		} else {
+		  res += str[p:i]
+		}
+
+		ch = string(str[i])
+		cnt = 1
+  	        p = i
+	}
+	
+	if p < n {
+	 res += str[p:n];
+	}
+	return res, nil;
+}
+
+func main() {
+	var str string = "as111111222as";
+	var s, er = encode(str);
+	if er == nil {
+		fmt.Println("Encoding: ", str, " is ", s)
+		fmt.Println("Decoding: ", s, " is ", decode(s))
+	}
+	
+}
