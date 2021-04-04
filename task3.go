3b8f4d77334bee12b181345e304d06ca93ada00e
jack198417 <j.atadjanov@gmail.com>
2021-04-04 23:48:16 +0500

    Create task3.go

---
+++ task3.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	str := "Are you from UK? No, I am from Uzb. It is in Middle Asia. You should know about Uzb";
+	if (strings.Trim(str, " ") == ""){
+		return;
+	}
+	str = strings.ToLower(str)
+	b := regexp.MustCompile(`,|( )|-|!|\.|\?`)
+	items := b.Split(str, -1)
+	mItems := map[string]int{}
+	word := ""
+	for i := 0; i < len(items); i++{
+	        word = strings.Trim(items[i], " ")
+		if len(word) > 0 {
+		  mItems[word]++  
+		}
+	}
+	fmt.Println(mItems)
+}
