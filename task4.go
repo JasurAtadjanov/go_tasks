2898227ffb635a9ea2169126e150dafd8ed5a347
jack198417 <j.atadjanov@gmail.com>
2021-04-07 01:03:15 +0500

    Update task4.go

--- task4.go
+++ task4.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fmt"
 	"strings"
+	"math/rand"
 )
 var pos string = "AZaz";
+var deltaUpper byte = pos[1] - pos[0]
+var deltaLower byte = pos[3] - pos[2]
 
-func encode(str string) string {
+func encode(str string, step byte) string {
 	var b strings.Builder
+	var ch byte;
 	for i := 0; i < len(str); i++{
-	  if (str[i] >= pos[0] && str[i] < pos[1]) ||
-	     (str[i] >= pos[2] && str[i] < pos[3]){
-	    b.WriteString(string(str[i] + 1))
-	  } else if (str[i] == pos[1]){
-	    b.WriteString(string(pos[0]))
-	  } else if (str[i] == pos[3]){
-	    b.WriteString(string(pos[2]))
+	  ch = str[i] + step;
+	  // if uppercase
+	  if (str[i] >= pos[0] && str[i] <= pos[1]){
+	    if ch > pos[1] {
+ 	       b.WriteString(string(ch - deltaUpper))
+	    }else if ch < pos[0] {
+ 	       b.WriteString(string(ch + deltaUpper))
+	    } else {
+ 	       b.WriteString(string(ch))
+	    }
+	  // if lower case  
+  	  } else if (str[i] >= pos[2] && str[i] <= pos[3]){
+	    if ch > pos[3] {
+ 	       b.WriteString(string(ch - deltaLower))
+	    } else if ch < pos[2] {
+ 	       b.WriteString(string(ch + deltaLower))
+	    } else {
+ 	       b.WriteString(string(ch))
+	    }	  
+	  
 	  } else {
   	    b.WriteString(string(str[i]))
 	  }
@@ -24,26 +41,38 @@ func encode(str string) string {
 	return b.String()
 }
 
-func decode(str string) string {
-	var b strings.Builder
-	for i := 0; i < len(str); i++{
-	  if (str[i] > pos[0] && str[i] <= pos[1]) ||
-	     (str[i] > pos[2] && str[i] <= pos[3]){
-	    b.WriteString(string(str[i] - 1))
-	  } else if (str[i] == pos[0]){
-	    b.WriteString(string(pos[1]))
-	  } else if (str[i] == pos[2]){
-	    b.WriteString(string(pos[3]))
-	  } else {
-  	    b.WriteString(string(str[i]))
-	  }
-	}
-
-	return b.String()
+func decode(str string, step byte) string {
+	return encode(str, 0 - step);
 }
 
 func main() {
-	str := "Decoding string by Sezar method!";
-	fmt.Println(encode(str));
-	fmt.Println(decode(encode(str)));
+        var key byte = byte(rand.Intn(1000)%26)
+  	str := "The default number generator is deterministic, so it will produce the same sequence of numbers each time by default. To produce varying sequences, give it a seed that changes. Note that this is not safe to use for random numbers you intend to be secret, use crypto/rand for those.";
+	
+	test := [5]string{"is", "so", "Will", "Same", "Numbers"}
+	// make test strings lower case
+	vTest := ",";
+	for i := 0; i < 5; i++{
+	  vTest += strings.ToLower(test[i]) + ",";
+	}
+
+	dStr := encode(str, key);
+	s := strings.Split(dStr, " ");
+
+	sTmp := ""
+	var j byte
+	for j = 0; j < 27; j++ {
+	  testCnt := 5;
+	  for i := 0; i < len(s); i++{
+  	        sTmp = "," + decode(s[i], j)+ ",";
+		if strings.Contains(vTest, sTmp){
+			testCnt--;
+			if testCnt < 1 {
+			 fmt.Println("Key=", j);
+			 fmt.Println("Decoded=", decode(dStr, j)); 
+			 return;
+			}
+		}
+	  }
+	}
 }
