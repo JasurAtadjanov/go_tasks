d9b0067a60305d254af460d0727e41b20707319b
jack198417 <j.atadjanov@gmail.com>
2021-04-15 05:51:04 +0500

    Update task3.go

--- task3.go
+++ task3.go
@@ -7,20 +7,28 @@ import (
 )
 
 func main() {
-	str := "Are you from UK? No, I am from Uzb. It is in Middle Asia. You should know about Uzb";
-	if (strings.Trim(str, " ") == ""){
-		return;
+	str := "Are you from UK? No, I am from Uzb. It is in Middle Asia. You should know about Uzb"
+	if strings.Trim(str, " ") == "" {
+		return
 	}
 	str = strings.ToLower(str)
 	b := regexp.MustCompile(`,|( )|-|!|\.|:|\?`)
 	items := b.Split(str, -1)
 	mItems := map[string]int{}
 	word := ""
-	for i := 0; i < len(items); i++{
-	        word = strings.Trim(items[i], " ")
+	for i := 0; i < len(items); i++ {
+		word = strings.Trim(items[i], " ")
 		if len(word) > 0 {
-		  mItems[word]++  
+			mItems[word]++
 		}
 	}
-	fmt.Println(mItems)
+
+	// fixing order and display format issue
+	for i := 0; i < len(items); i++ {
+		word = strings.Trim(items[i], " ")
+		if val, ok := mItems[word]; ok {
+			fmt.Printf("%s (%d) ", word, val)
+		}
+	}
+
 }
